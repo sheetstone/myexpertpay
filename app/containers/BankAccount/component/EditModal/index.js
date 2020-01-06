829508faabc8accc39c2690606f76ad3c169b37e
Hong Zhang <hong.zhang3@xerox.com>
2020-01-06 13:29:46 -0600

    update

--- app/containers/BankAccount/component/EditModal/index.js
+++ app/containers/BankAccount/component/EditModal/index.js
@@ -2,22 +2,55 @@
  * Bank List
  */
 import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import { FormattedMessage } from 'react-intl';
+import { Modal, Button, Form, Col, Row } from 'react-bootstrap';
+
+import messages from '../../messages';
 
 export default function EditBankAccount() {
   return (
     <Modal.Dialog>
       <Modal.Header closeButton>
-        <Modal.Title>Modal title</Modal.Title>
+        <Modal.Title>
+          <FormattedMessage {...messages.addBankTitle} />
+        </Modal.Title>
       </Modal.Header>
 
       <Modal.Body>
-        <p>Modal body text goes here.</p>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>
+            <FormattedMessage {...messages.rountingNumber} />
+          </Form.Label>
+          <Form.Control type="number" />
+        </Form.Group>
+        <Row>
+          <Col>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>
+                <FormattedMessage {...messages.accountNumber} />
+              </Form.Label>
+              <Form.Control type="number" />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>
+                <FormattedMessage {...messages.accountNumberConfirmation} />
+              </Form.Label>
+              <Form.Control type="number" />
+            </Form.Group>
+          </Col>
+        </Row>
+        <p>
+          <FormattedMessage {...messages.addBankMessage} />
+        </p>
       </Modal.Body>
 
       <Modal.Footer>
         <Button variant="secondary">Close</Button>
-        <Button variant="primary">Save changes</Button>
+        <Button variant="primary" type="submit">
+          <FormattedMessage {...messages.addBankSubmit} />
+        </Button>
       </Modal.Footer>
     </Modal.Dialog>
   );
